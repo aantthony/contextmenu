0ca6fef29aa427f8771bd6f467f7bb005f94baa3
Anthony <antscape@gmail.com>
2012-03-02 16:50:51 +1100

    nice

--- contextmenu.js
+++ contextmenu.js
@@ -1,4 +1,6 @@
-;!function(d, window, undefined){
+var contextmenu = function(d, window, undefined){
+	var nativeSupport = /Firefox/.test(navigator.userAgent);
+	
 	function guid() {
 		return (0 | 10000000 * Math.random()).toString(30) + (0 | 10000000 * Math.random()).toString(30) + (0 | 10000000 * Math.random()).toString(30);
 	}
@@ -18,37 +20,17 @@
 			top: curtop
 		};
 	}
-	var menustack = [];
-	var preview; //Extension to the menustack.
-	var _ = d.createElement("div");
-	d.body.className+=" osx10_7";
-	_.className = "_contextmenu_screen_";
-	var menus_dom = d.getElementsByTagName("menu");
-	var timeout = 150;
-	_.addEventListener("mousedown", function(){
-		menustack = [];
+	function _mousedown(e){
 		_.style.opacity = "0.0";
 		setTimeout(function () {
+			while(menustack.length) {
+				popmenu(false);
+			}
 			_.style.display = "none";
 			_.style.opacity = "1.0";
+			
 		}, timeout);
-	});
-	_.addEventListener("contextmenu", function (e){
-		menustack = [];
-		e.preventDefault();
-		return false;
-	});
-	
-	_.addEventListener("mouseover", function(e){
-		if(e.target !== _) {
-			return;
-		}
-		//Hide preview menu.
-		if(preview){
-			hideMenu(preview);
-			preview = undefined;
-		}
-	});
+	}
 	function hideMenu(menu, fade){
 		if(fade) {
 			menu.style.opacity = "0.0";
@@ -66,23 +48,22 @@
 		}
 	}
 	
-	var i, l;
-	var menus = [];
-	for(i = 0, l = menus_dom.length; i < l; i++) {
-		menus[i] = menus_dom[i];
-	}
-	for(i = 0, l = menus.length; i < l; i++) {
-		var menu = menus[i];
+	function prepareMenu(menu) {
 		var p = menu.parentNode;
 		menu.addEventListener("mouseover", onmouseover, false);
+		menu.addEventListener("mousedown", onmousedown);
 		if(p.nodeName === "MENU") {
 			var clone = d.createElement("menuitem");
 			menu.className += " submenu";
 			clone.className =  menu.className;
 			clone.setAttribute("label", menu.getAttribute("label"));
-			var node_id = "_anon_menu_" + guid();
-			clone.setAttribute("contextmenu", node_id);
-			menu.id = node_id;
+			if(!menu.id) {
+				var node_id = "_anon_menu_" + guid();
+				clone.setAttribute("contextmenu", node_id);
+				menu.id = node_id;
+			} else {
+				clone.setAttribute("contextmenu", menu.id);
+			}
 			clone.addEventListener("mouseover", onsubcontextmenu);
 			clone.addEventListener("mouseout", onsubcontextmenuout);
 			//clone.addEventListener("mouseout", onsubcontextmenu); //to another node
@@ -94,17 +75,19 @@
 			_.appendChild(menu);
 		}
 	}
-	d.body.appendChild(_);
-	
 	function mother(name, node) {
 		if(node.nodeName === name) {
 			return node;
 		}
 		return mother(name, node.parentNode || {nodeName: name});
 	}
+	function onmousedown(e){
+		e.stopPropagation();
+		e.preventDefault();
+		return false;
+	}
 	function onmouseover(e) {
 		var menu = mother("MENU", e.target);
-		console.log("ENTER");
 		if(preview){
 			if(preview === menu) {
 				menustack.push(menu);
@@ -118,7 +101,6 @@
 		//Pop while not menu
 		var a = menustack.indexOf(menu);
 		for(i = msl-1; i > a; i--) {
-			console.log("pop", menustack[menustack.length-1]);
 			popmenu();
 		}
 		
@@ -134,7 +116,6 @@
 		}
 		var i;
 		for(i = msl-1; i > a; i--) {
-			console.log("Popped",menustack[menustack.length-1],"because", menu, "is not an ancestor of", top);
 			popmenu();
 		}
 		
@@ -154,8 +135,6 @@
 		popmenu();
 	}
 	function onsubcontextmenuout(e){
-		console.log("out from", e);
-		window.e=e;
 		var menu = mother("MENU", e.toElement);
 		if(menu === preview) {
 			return false;
@@ -184,22 +163,147 @@
 		e.stopPropagation();
 		return false;
 	}
-	function oncontextmenu(e){
-		var menu = document.getElementById(e.srcElement.getAttribute("contextmenu"));
+	function initContextMenu(menu, x, y) {
 		menustack.push(menu);
-		menu.style.top = (e.pageY - 5) + "px";
-		menu.style.left = e.pageX + "px";
+		menu.style.top = (y - 5) + "px";
+		menu.style.left = x + "px";
 		menu.style.display = "inline-block";
 		_.style.display = "block";
+	}
+	
+	function oncontextsheet(e){
+		window.e = e;
+		window.offset = offset;
+		var pos = offset(e.target);
+		var menu = document.getElementById(e.srcElement.getAttribute("contextmenu"));
+		t = new Date();
+		initContextMenu(menu, pos.left, pos.top + pos.height + 8);
 		e.preventDefault();
 		return false;
 	}
-	function onselectchange(e) {
-		console.log("ABC");
+	function oncontextmenu(e){
+		var menu = document.getElementById(e.srcElement.getAttribute("contextmenu"));
+		t = new Date();
+		initContextMenu(menu, e.pageX, e.pageY);
+		e.preventDefault();
+		return false;
 	}
+	
+if(!nativeSupport){
+	var menustack = [];
+	var preview; //Extension to the menustack.
+	var _ = d.createElement("div");
+	d.body.className +=" osx10_7";
+	_.className = "_contextmenu_screen_";
+	var menus_dom = d.getElementsByTagName("menu");
+	var timeout = 150;
+	var t = 0;
+	
+	
+	var i, l;
+	var menus = [];
+	for(i = 0, l = menus_dom.length; i < l; i++) {
+		menus[i] = menus_dom[i];
+	}
+	for(i = 0, l = menus.length; i < l; i++) {
+		prepareMenu(menus[i]);
+	}
+	d.body.appendChild(_);
+	
+	
+	_.addEventListener("mousedown", function(e){
+		_mousedown(e);
+	});
+	
+	_.addEventListener("mouseup", function(e){
+		var menuitem = e.target;
+		if(menuitem.nodeName === "MENUITEM" && menuitem.hasAttribute("contextmenu")) {
+			return false;
+		}
+		if(new Date() - t < 300) {
+			return;
+		}
+		_mousedown(e);
+	});
+	_.addEventListener("contextmenu", function (e){
+		menustack = [];
+		e.preventDefault();
+		return false;
+	});
+	
+	_.addEventListener("mouseover", function(e){
+		if(e.target !== _) {
+			return;
+		}
+		//Hide preview menu.
+		if(preview){
+			hideMenu(preview);
+			preview = undefined;
+		}
+	});
+	
+	
 	var linkers = d.getElementsByClassName("contextmenu");
 	for(i = 0, l = linkers.length; i < l; i++) {
 		var element = linkers[i];
-		element.addEventListener("contextmenu", oncontextmenu);
+		if(element.nodeName === "INPUT") {
+			element.addEventListener("click", oncontextsheet);
+			//Right clicking buttons. Bad idea?
+			element.addEventListener("contextmenu", oncontextsheet);
+		} else {
+			element.addEventListener("contextmenu", oncontextmenu);
+		}
 	}
+}
+	var _contextmenu = function(x){
+		var i, l;
+		var menu = document.createElement("menu");
+		for(i = 0, l = x.length; i < l; i++) {
+			var xi = x[i];
+			if(xi.children) {
+				var submenu = _contextmenu(xi.children);
+				submenu.setAttribute("label", xi.label);
+				menu.appendChild(submenu);
+			} else {
+				var menuitem = document.createElement("menuitem");
+				menuitem.setAttribute("label", xi.label);
+				xi.onclick && (menuitem.onclick = xi.onclick);
+				xi.icon && (menuitem.icon = xi.icon);
+				
+				menu.appendChild(menuitem);
+			}
+		}
+		return menu;
+	};
+	var contextmenu = function(x){
+		var menu = _contextmenu(x);
+		d.body.appendChild(menu);
+		if(nativeSupport){
+			return menu;
+		}
+		var submenus = menu.getElementsByTagName("menu");
+		var menus = [];
+		menus.push(menu);
+		var i, l;
+		for(i = 0, l = submenus.length; i < l; i++) {
+			menus.push(submenus[i]);
+		}
+		for(i = 0, l = menus.length; i < l; i++) {
+			prepareMenu(menus[i]);
+		}
+		
+		return menu;
+	};
+	
+	contextmenu.show = function(menu, x, y){
+		if(nativeSupport){
+			throw("Not supported");
+		}
+		if(typeof id === "string") {
+			menu = document.getElementById(menu);
+		}
+		initContextMenu(menu, x, y);
+		return this;
+	}
+	return contextmenu;
 }(document, window);
