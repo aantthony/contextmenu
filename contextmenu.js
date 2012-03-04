499ff610846320d6ce2436a780aa76f6a5250ed2
Anthony <antscape@gmail.com>
2012-03-04 17:25:07 +1100

    all working

--- contextmenu.js
+++ contextmenu.js
@@ -1,49 +1,59 @@
-var contextmenu = function(d, window, undefined){
-	var nativeSupport = /Firefox/.test(navigator.userAgent);
-	
-	function nextTick(callback){
+var contextmenu = function (d, window) {
+	"use strict";
+	var nativeSupport = ((d.body.contextMenu === null) && window.HTMLMenuItemElement !== undefined),
+		lastX,
+		lastY,
+		mousedown_timeout,
+		overlay,
+		menustack,
+		preview,
+		timeout,
+		t,
+		preview_show_timer;
+
+	function nextTick(callback) {
 		setTimeout(callback, 0);
 	}
 	function guid() {
 		return (0 | 10000000 * Math.random()).toString(30) + (0 | 10000000 * Math.random()).toString(30) + (0 | 10000000 * Math.random()).toString(30);
 	}
-	// Only use sleep if you know what you are doing.
-	// WIll max out 100% if using cpu_hog = true!
-	function sleep(time, cpu_hog) {
-		var t_end = (new Date() -0 )+ time;
-		while(new Date()-0 < t_end) {
-			if(!cpu_hog){
-				try{
-					sleep_work();
-				}catch(ex){
-					console.error(ex);
-				}
-			}
-		}
-		return (new Date()-0) - t_end;
-	}
 	function sleep_work() {
-		var resource;
-		if(typeof ActiveXObject == 'undefined') {
+		var resource, response;
+		if (typeof ActiveXObject === 'undefined') {
 			resource = new XMLHttpRequest();
-		} else{
+		} else {
 			resource = new ActiveXObject("Microsoft.XMLHTTP");
 		}
 		try {
 			resource.open('GET', '/', false);
 			resource.send(null);
 			response = resource.responseText;
-		}catch(e){
-			
+		} catch (e) {
+			// Ignore
 		}
 	}
-	function offset(obj) {
-		if(obj.getClientRects) {
-			var rect = obj.getClientRects()[0];
-			return rect;
+	// Only use sleep if you know what you are doing.
+	// WIll max out 100% if using cpu_hog = true!
+
+	function sleep(time, cpu_hog) {
+		var t_end = (new Date().getTime()) + time;
+		while (new Date().getTime() < t_end) {
+			if (!cpu_hog) {
+				try {
+					sleep_work();
+				} catch (ex) {
+					console.error(ex);
+				}
+			}
 		}
-		var curleft  = 0;
-		var curtop = 0;
+		return (new Date().getTime()) - t_end;
+	}
+	function offset(obj) {
+		var curleft = 0,
+			curtop = 0;
+		if (obj.getClientRects) {
+			return obj.getClientRects()[0];
+		}
 		do {
 			curleft += obj.offsetLeft;
 			curtop += obj.offsetTop;
@@ -53,350 +63,378 @@ var contextmenu = function(d, window, undefined){
 			top: curtop
 		};
 	}
-	var mousedown_timeout;
-	function _mousedown(e){
-		console.warn("_mousedown");
-		_.style.opacity = "0.0";
-		mousedown_timeout = setTimeout(function () {
-			while(menustack.length) {
-				popmenu(false);
-			}
-			_.style.display = "none";
-			_.style.opacity = "1.0";
-			
-		}, timeout);
-	}
-	window._mousedown = _mousedown;
-	function hideMenu(menu, fade){
-		if(fade) {
-			throw("no need to fade");
-			var c = menu.className;
-			menu.className += " fadeout"
-			nextTick(function (){
+	function hideMenu(menu, fade) {
+		if (fade) {
+			throw ("no need to fade");
+			nextTick(function () {
 				menu.style.opacity = "0.0";
 			});
-			
 			setTimeout(function () {
 				menu.style.display = "none";
-				menu.className = c;
 				menu.style.opacity = "1.0";
 			}, timeout);
-		}else {
+		} else {
 			menu.style.display = "none";
 		}
-		
 		var launcher = menu.launcher;
-		if(launcher) {
+		if (launcher) {
 			launcher.removeAttribute("open");
 		}
 	}
-	
-	function prepareMenu(menu) {
-		var p = menu.parentNode;
-		menu.addEventListener("mouseover", onmouseover, false);
-		menu.addEventListener("mousedown", onmousedown);
-		if(p.nodeName === "MENU") {
-			var clone = d.createElement("menuitem");
-			menu.className += " submenu";
-			clone.className =  menu.className;
-			clone.setAttribute("label", menu.getAttribute("label"));
-			if(!menu.id) {
-				var node_id = "_anon_menu_" + guid();
-				clone.setAttribute("contextmenu", node_id);
-				menu.id = node_id;
-			} else {
-				clone.setAttribute("contextmenu", menu.id);
+	function popmenu() {
+		var top = menustack.pop();
+		hideMenu(top);
+		return top;
+	}
+	function mouseend(e) {
+		overlay.style.opacity = "0.0";
+		mousedown_timeout = setTimeout(function () {
+			while (menustack.length) {
+				popmenu(false);
 			}
-			clone.addEventListener("mouseover", onsubcontextmenu);
-			clone.addEventListener("mouseout", onsubcontextmenuout);
-			//clone.addEventListener("mouseout", onsubcontextmenu); //to another node
-			//menu.addEventListener("mouseout", onsubmouseout, false);
-			p.replaceChild(clone, menu);
-			_.appendChild(menu);
-		}else {
-			p.removeChild(menu);
-			_.appendChild(menu);
-		}
+			overlay.style.display = "none";
+			overlay.style.opacity = "1.0";
+		}, timeout);
+	}
+	function showMenu(menu) {
+		menu.style.display = "inline-block";
+	}
+	function menuoncontextmenu(e) {
+		e.stopPropagation();
+		e.preventDefault();
 	}
 	function mother(name, node) {
-		if(node.nodeName === name) {
+		if (node.nodeName === name) {
 			return node;
 		}
 		return mother(name, node.parentNode || {nodeName: name});
 	}
-	function onmousedown(e){
-		e.stopPropagation();
-		e.preventDefault();
-		return false;
-	}
 	function onmouseover(e) {
-		var menu = mother("MENU", e.target);
-		if(preview){
-			if(preview === menu) {
+		//This event is also triggered after mouseover on submenu menuitems
+		var menu = mother("MENU", e.target),
+			msl,
+			a,
+			i;
+		if (preview) {
+			if (preview === menu) {
 				menustack.push(menu);
 				preview = undefined;
 				return;
 			}
 			console.error("SHOULD NOT BE A PREVIEW");
 		}
-		var msl = menustack.length;
-		var top = menustack[msl-1];
+		msl = menustack.length;
 		//Pop while not menu
-		var a = menustack.indexOf(menu);
-		for(i = msl-1; i > a; i--) {
+		a = menustack.indexOf(menu);
+		for (i = msl - 1; i > a; i--) {
 			popmenu();
 		}
-		
-		return;
-		if(top === menu) {
-			//Hmm.. It's opening now?
-			return;
-		}
-		//Pop while menu is not an ancestor of top;
-		var a = menustack.indexOf(menu);
-		if(a === -1){
-			throw("??? Should not be visible if it isn't an ancestor! It is fading away?");
-		}
-		var i;
-		for(i = msl-1; i > a; i--) {
-			popmenu();
-		}
-		
 	}
-	function popmenu(){
-		var top = menustack.pop();
-		hideMenu(top);
-		return top;
-	}
-	function onsubmouseout(e){
-		var menu = e.srcElement;
-		return false;
-		if(menu.nodeName !== "MENU"){
-			return false;
+	function onmousedown(e) {
+		if (e.target.nodeName === "MENU") {
+			if (e.offsetX === 0) {
+				//Left 1px border shouldn't be included.
+				//Close the menu: (bubble event)
+				return;
+			}
 		}
-		return false;
-		popmenu();
-	}
-	function onsubcontextmenuout(e){
-		var menu = mother("MENU", e.toElement);
-		if(menu === preview) {
-			return false;
-		}
-		hideMenu(preview);
-		preview = undefined;
-	}
-	function onsubcontextmenu(e){
-		var menuitem = e.srcElement;
-		var menu = document.getElementById(menuitem.getAttribute("contextmenu"));
-		menu.launcher = menuitem;
-		if(preview && preview !== menu){
-			hideMenu(preview);
-		}
-		preview = menu;
-		var pos = offset(menuitem);
-		menu.style.top = (pos.top - 5) + "px";
-		menu.style.left = (pos.left + pos.width - 1) + "px";
-		menu.style.display = "inline-block";
-		_.style.display = "block";
-		menuitem.default_className = menuitem.className;
-		if(!menuitem.opensubmenu) {
-			menuitem.setAttribute("open", true);
-		}
-		e.preventDefault();
 		e.stopPropagation();
+		e.preventDefault();
 		return false;
+	}
+	function onsubcontextmenuout(e) {
+		preview_show_timer = clearTimeout(preview_show_timer);
+		var menu = mother("MENU", e.toElement);
+		if (menu === preview) {
+			return false;
+		}
+		if (preview) {
+			hideMenu(preview);
+			preview = undefined;
+		}
+	}
+	function onsubcontextmenu(e) {
+		preview_show_timer = clearTimeout(preview_show_timer);
+		preview_show_timer = setTimeout(function () {
+			var menuitem = e.srcElement,
+				menu = menuitem.contextMenu,
+				pos;
+			menu.launcher = menuitem;
+			if (preview && preview !== menu) {
+				hideMenu(preview);
+			}
+			preview = menu;
+			pos = offset(menuitem);
+			menu.style.top = (pos.top - 5) + "px";
+			menu.style.left = (pos.left + pos.width - 1) + "px";
+			menu.style.display = "inline-block";
+			overlay.style.display = "block";
+			if (!menuitem.opensubmenu) {
+				menuitem.setAttribute("open", true);
+			}
+		}, 200);
+		//Don't stop propagation, the event bubbles to the <menu /> mouseover handler
+	}
+	function stopPropagation(e) {
+		e.stopPropagation();
+	}
+	function prepareMenu(menu) {
+		var p = menu.parentNode,
+			clone;
+		menu.addEventListener("mouseover", onmouseover, false);
+		menu.addEventListener("mousedown", onmousedown);
+		menu.addEventListener("contextmenu", menuoncontextmenu);
+		if (p.nodeName === "MENU") {
+			clone = d.createElement("menuitem");
+			menu.classList.add("submenu");
+			clone.className =  menu.className;
+			clone.setAttribute("label", menu.getAttribute("label"));
+			clone.contextMenu = menu;
+			clone.addEventListener("mouseover", onsubcontextmenu);
+			clone.addEventListener("mouseout", onsubcontextmenuout);
+
+			//This isn't right. It should end the context menu if it was a hold, i.e.,
+			//follow the same rules as the parent context menu.
+			clone.addEventListener("mouseup", stopPropagation);
+			p.replaceChild(clone, menu);
+			overlay.appendChild(menu);
+		} else {
+			p.removeChild(menu);
+			overlay.appendChild(menu);
+		}
 	}
 	function initContextMenu(menu, x, y) {
 		t = new Date();
 		menustack.push(menu);
 		menu.style.top = (y - 5) + "px";
 		menu.style.left = x + "px";
-		menu.style.display = "inline-block";
-		_.style.display = "block";
+		showMenu(menu);
+		overlay.style.display = "block";
 	}
-	
-	function oncontextsheet(e){
-		window.e = e;
-		window.offset = offset;
-		var pos = offset(e.target);
-		var menu = document.getElementById(e.srcElement.getAttribute("contextmenu"));
+	function oncontextsheet(e) {
+		var pos = offset(e.target),
+			menu = d.getElementById(e.srcElement.getAttribute("contextmenu"));
 		t = new Date();
 		initContextMenu(menu, pos.left, pos.top + pos.height + 8);
 		e.preventDefault();
 		return false;
 	}
-	function oncontextmenu(e){
-		var menu = document.getElementById(e.srcElement.getAttribute("contextmenu"));
+	function contextMenufor(node) {
+		if (!node || !node.hasAttribute) {
+			return;
+		}
+		if (node.hasAttribute("contextmenu")) {
+			return node.getAttribute("contextmenu");
+		}
+		return contextMenufor(node.parentNode);
+	}
+	function oncontextmenu(e) {
+		var menu = d.getElementById(contextMenufor(e.srcElement)),
+			x = e.clientX,
+			y = e.clientY;
 		initContextMenu(menu, e.clientX, e.clientY);
-		e.preventDefault();
-		return false;
-	}
-	
-if(!nativeSupport){
-	var menustack = [];
-	var preview; //Extension to the menustack.
-	var _ = d.createElement("div");
-	var os_code = "osx10_7";
-	if(/Mac/.test(navigator.userAgent)) {
-		os_code = "osx10_7";
-	}else if(/Win/.test(navigator.userAgent)) {
-		os_code = "win7";
-	}
-	
-	d.body.className += " " + os_code;
-	_.className = "_contextmenu_screen_";
-	var menus_dom = d.getElementsByTagName("menu");
-	var timeout = 150;
-	var t = 0;
-	
-	
-	var i, l;
-	var menus = [];
-	for(i = 0, l = menus_dom.length; i < l; i++) {
-		menus[i] = menus_dom[i];
-	}
-	for(i = 0, l = menus.length; i < l; i++) {
-		prepareMenu(menus[i]);
-	}
-	d.body.appendChild(_);
-	
-	
-	_.addEventListener("mousedown", function(e){
-		_mousedown(e);
-	});
-	var mouseup_wait_for_me = 0;
-	_.addEventListener("mouseup", function(e){
-		if(mouseup_wait_for_me) {
-			return;
-		}
-		var menuitem = e.target;
-		if(menuitem.nodeName === "MENUITEM" && menuitem.hasAttribute("contextmenu")) {
-			return false;
-		}
-		if(new Date() - t < 300) {
-			return;
-		}
-		_mousedown(e);
-	});
-	_.addEventListener("mousewheel", function(e){
+		lastX = x;
+		lastY = y;
 		e.preventDefault();
 		e.stopPropagation();
 		return false;
-	}, true);
-	function contextMenuFor(node) {
-		if(!node || !node.hasAttribute) {
-			return;
-		}
-		if(node.hasAttribute("contextmenu")){
-			return node.getAttribute("contextmenu");
-		}
-		return contextMenuFor(node.parentNode);
 	}
-	_.addEventListener("contextmenu", function (e){
-		_.style.display = "none";
-		var node = document.elementFromPoint(e.clientX, e.clientY);
-		var contextmenu = contextMenuFor(node);
-		if(contextmenu) {
-			_.style.display = "block";
-			clearTimeout(mousedown_timeout);
+	function inititalize() {
+		menustack = [];
+		overlay = d.createElement("div");
+		var os_code = "osx10_7",
+			mouseup_wait_for_me = 0,
+			i,
+			l;
+		if (/Mac/.test(navigator.userAgent)) {
+			os_code = "osx10_7";
+		} else if (/Win/.test(navigator.userAgent)) {
+			os_code = "win7";
+		}
+		d.body.classList.add(os_code);
+		overlay.className = "_contextmenu_screen_";
+
+		timeout = 150;
+		t = 0;
+		d.body.appendChild(overlay);
+		overlay.addEventListener("mousedown", function (e) {
+			mouseend(e);
+		});
+		overlay.addEventListener("mouseup", function (e) {
+			if (mouseup_wait_for_me) {
+				return;
+			}
+			var menuitem = e.target;
+			if (menuitem.nodeName === "MENUITEM" && menuitem.hasAttribute("contextmenu")) {
+				return false;
+			}
+			if (new Date() - t < 300) {
+				return;
+			}
+			if (menuitem.nodeName === "MENUITEM") {
+				//Click animation.
+				setTimeout(function () {
+					menuitem.style.background = "white";
+					setTimeout(function () {
+						menuitem.style.background = "";
+						setTimeout(function () {
+							mouseend(e);
+						}, 30);
+					}, 80);
+				}, 10);
+			}
+		});
+		overlay.addEventListener("mousewheel", function (e) {
 			e.preventDefault();
-			var oldstack = menustack;
-			var menu = document.getElementById(contextmenu);
-			
-			mouseup_wait_for_me++;
-			setTimeout(function(){
-				while(menustack.length) {
-					var m = menustack.pop();
-					hideMenu(m);
-				}
-				_.style.display = "none";
-				_.style.opacity = "1.0";
-				_.style.display = "block";
-				nextTick(function(){
-					mouseup_wait_for_me--;
-					initContextMenu(menu, e.clientX, e.clientY);
-				});
-			}, timeout);
+			e.stopPropagation();
 			return false;
-		}else{
-			_.style.opacity = "1.0";
-			_.style.display = "block";
-			clearTimeout(mousedown_timeout);
-			nextTick(_mousedown);
-			sleep(timeout, true);
+		}, true);
+		overlay.addEventListener("contextmenu", function (e) {
+			overlay.style.display = "none";
+			var node = d.elementFromPoint(e.clientX, e.clientY),
+				contextmenu = contextMenufor(node),
+				menu,
+				x,
+				y,
+				dx,
+				dy;
+			if (contextmenu) {
+				overlay.style.display = "block";
+				clearTimeout(mousedown_timeout);
+				e.preventDefault();
+				menu = d.getElementById(contextmenu);
+				if (menustack[0] === menu) {
+					//Same menu:
+					x = e.clientX;
+					y = e.clientY;
+					dx = x - lastX;
+					dy = y - lastY;
+					lastX = x;
+					lastY = y;
+					if (dx * dx + dy * dy < 50) {
+						overlay.style.opacity = "1.0";
+						overlay.style.display = "block";
+						clearTimeout(mousedown_timeout);
+						nextTick(mouseend);
+						return;
+					}
+				}
+				mouseup_wait_for_me++;
+				setTimeout(function () {
+					while (menustack.length) {
+						var m = menustack.pop();
+						hideMenu(m);
+					}
+					overlay.style.display = "none";
+					overlay.style.opacity = "1.0";
+					overlay.style.display = "block";
+					nextTick(function () {
+						mouseup_wait_for_me--;
+						initContextMenu(menu, e.clientX, e.clientY);
+					});
+				}, timeout);
+				return false;
+			} else {
+				overlay.style.opacity = "1.0";
+				overlay.style.display = "block";
+				clearTimeout(mousedown_timeout);
+				nextTick(mouseend);
+			}
+		});
+		overlay.addEventListener("mouseover", function (e) {
+			if (e.target !== overlay) {
+				return;
+			}
+			//Hide preview menu.
+			preview_show_timer = clearTimeout(preview_show_timer);
+			if (preview) {
+				hideMenu(preview);
+				preview = undefined;
+			}
+		});
+	}
+	function attachEventsToAllMenus() {
+		var menus_dom = d.getElementsByTagName("menu"),
+			i,
+			l,
+			menus = [];
+		for (i = 0, l = menus_dom.length; i < l; i++) {
+			menus[i] = menus_dom[i];
 		}
-	});
-	
-	_.addEventListener("mouseover", function(e){
-		if(e.target !== _) {
-			return;
-		}
-		//Hide preview menu.
-		if(preview){
-			hideMenu(preview);
-			preview = undefined;
-		}
-	});
-	
-	
-	var linkers = d.getElementsByClassName("contextmenu");
-	for(i = 0, l = linkers.length; i < l; i++) {
-		var element = linkers[i];
-		if(element.nodeName === "INPUT") {
-			element.addEventListener("click", oncontextsheet);
-			//Right clicking buttons. Bad idea?
-			element.addEventListener("contextmenu", oncontextsheet);
-		} else {
-			element.addEventListener("contextmenu", oncontextmenu);
+		for (i = 0, l = menus.length; i < l; i++) {
+			prepareMenu(menus[i]);
 		}
 	}
-}
-	var _contextmenu = function(x){
-		var i, l;
-		var menu = document.createElement("menu");
-		for(i = 0, l = x.length; i < l; i++) {
-			var xi = x[i];
-			if(xi.children) {
-				var submenu = _contextmenu(xi.children);
+	function hookUpContextMenus() {
+		var linkers = d.querySelectorAll("[contextmenu]"),
+			element,
+			i,
+			l;
+		for (i = 0, l = linkers.length; i < l; i++) {
+			element = linkers[i];
+			if (element.nodeName === "INPUT") {
+				element.addEventListener("click", oncontextsheet);
+				//Right clicking buttons. Bad idea?
+				element.addEventListener("contextmenu", oncontextsheet);
+			} else {
+				element.addEventListener("contextmenu", oncontextmenu);
+			}
+		}
+	}
+	if (!nativeSupport) {
+		inititalize();
+		attachEventsToAllMenus();
+		hookUpContextMenus();
+	}
+	function buildMenu(x) {
+		var menu = d.createElement("menu"),
+			i,
+			l,
+			xi,
+			submenu,
+			menuitem;
+		for (i = 0, l = x.length; i < l; i++) {
+			xi = x[i];
+			if (xi.children) {
+				submenu = buildMenu(xi.children);
 				submenu.setAttribute("label", xi.label);
 				menu.appendChild(submenu);
 			} else {
-				var menuitem = document.createElement("menuitem");
+				menuitem = d.createElement("menuitem");
 				menuitem.setAttribute("label", xi.label);
 				xi.onclick && (menuitem.onclick = xi.onclick);
 				xi.icon && (menuitem.icon = xi.icon);
-				
 				menu.appendChild(menuitem);
 			}
 		}
 		return menu;
-	};
-	var contextmenu = function(x){
-		var menu = _contextmenu(x);
+	}
+	function contextmenu(x) {
+		var menu = buildMenu(x),
+			submenus,
+			menus,
+			i,
+			l;
 		d.body.appendChild(menu);
-		if(nativeSupport){
+		if (nativeSupport) {
 			return menu;
 		}
-		var submenus = menu.getElementsByTagName("menu");
-		var menus = [];
-		menus.push(menu);
-		var i, l;
-		for(i = 0, l = submenus.length; i < l; i++) {
+		submenus = menu.getElementsByTagName("menu");
+		menus = [menu];
+		for (i = 0, l = submenus.length; i < l; i++) {
 			menus.push(submenus[i]);
 		}
-		for(i = 0, l = menus.length; i < l; i++) {
+		for (i = 0, l = menus.length; i < l; i++) {
 			prepareMenu(menus[i]);
 		}
-		mousedown_timeout
 		return menu;
-	};
-	
-	contextmenu.show = function(menu, x, y){
-		if(nativeSupport){
-			throw("Not supported");
+	}
+	contextmenu.show = function (menu, x, y) {
+		if (nativeSupport) {
+			throw ("Not supported");
 		}
-		if(typeof id === "string") {
-			menu = document.getElementById(menu);
+		if (typeof menu === "string") {
+			menu = d.getElementById(menu);
 		}
 		initContextMenu(menu, x, y);
 		return this;
