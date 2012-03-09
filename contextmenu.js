071e78635b3086d0519d0bf90cba0115a12b1fdf
Anthony <antscape@gmail.com>
2012-03-10 01:35:05 +1100

    ...

--- contextmenu.js
+++ contextmenu.js
@@ -1,21 +1,32 @@
+/*
+ * Contextmenu v0.1
+ * https://github.com/aantthony/contextmenu
+ *
+ * Released under the MIT license
+ * Copyright 2012
+ *
+ * Date: Sun Mar 4 2012
+ */
 (function (d, window) {
 	"use strict";
 	var nativeSupport = ((d.body.contextMenu === null) && window.HTMLMenuItemElement !== undefined),
-		lastX,
-		lastY,
-		mousedown_timeout,
-		overlay,
-		menustack,
-		preview,
-		timeout,
-		t,
-		preview_show_timer,
-		holding = false,
-		old_contextmenu = window.contextmenu;
+		lastX,				// Last position where root context menu was launched
+		lastY,				//  ''
+		mousedown_timeout,	// The timer which in timeout ms, will set the overlay with display: false
+		overlay,			// A div that covers the screen while contextmenus are visible
+		menustack,			// Stack of menu nodes open. menustack[0] is root. Does not include preview
+		preview,			// A menu node which is visible (due to hovering on a expandable menu(item))
+		timeout,			// The time (ms) taken to fade out.
+		t,					// The time of mousedown. Used to determine if it is a hold and relase menu.
+		preview_show_timer,	// Menu previews don't show instantly, but a few hundred ms later.
+		holding = false,	// Is the user doing a hold an release menu selection?
+		old_contextmenu = window.contextmenu;  // See: contextmenu.noConflict()
 
 	function nextTick(callback) {
 		setTimeout(callback, 0);
 	}
+
+	// Calculate the position of a dom node
 	function offset(obj) {
 		var curleft = 0,
 			curtop = 0;
@@ -32,6 +43,14 @@
 			top: curtop
 		};
 	}
+
+	function html(n){
+		if(n.get) {
+			return n.get(0);
+		}
+		return n;
+	}
+
 	function hideMenu(menu, fade) {
 		if (fade) {
 			throw ("no need to fade");
@@ -42,11 +61,15 @@
 			launcher.removeAttribute("open");
 		}
 	}
+
+	// Remove the topmost context menu from the stack
 	function popmenu() {
 		var top = menustack.pop();
 		hideMenu(top);
 		return top;
 	}
+
+	// Context menu operation complete. Triggered by multiple things.
 	function mouseend() {
 		overlay.style.opacity = "0.0";
 		mousedown_timeout = setTimeout(function () {
@@ -57,40 +80,52 @@
 			overlay.style.opacity = "1.0";
 		}, timeout);
 	}
+
 	function showMenu(menu) {
 		menu.style.display = "inline-block";
 	}
+
+	// Don't allow right clicking a context menu.
 	function menuoncontextmenu(e) {
+		// TODO: Right clicking an item should trigger a click event.
 		e.stopPropagation();
 		e.preventDefault();
 	}
-	function mother(name, node) {
+
+	// Search up the dom tree for the first ancestor with a given name.
+	function ancestor(name, node) {
 		if (node.nodeName === name) {
 			return node;
 		}
-		return mother(name, node.parentNode || {nodeName: name});
+		return ancestor(name, node.parentNode || {nodeName: name});
 	}
+
+	// menu.onmouseover
 	function onmouseover(e) {
 		//This event is also triggered after mouseover on submenu menuitems
-		var menu = mother("MENU", e.target),
+		var menu = ancestor("MENU", e.target),
 			msl,
 			a,
 			i;
 		if (preview) {
 			if (preview === menu) {
+				// It is no longer a preview anymore. Push it onto the stack
 				menustack.push(menu);
 				preview = undefined;
 				return;
 			}
+			// This shouldn't be triggered because while previews will be hidden instantly.
 			console.error("SHOULD NOT BE A PREVIEW");
 		}
 		msl = menustack.length;
-		//Pop while not menu
+		//Pop contextmenus until the focused menu is at the top.
 		a = menustack.indexOf(menu);
 		for (i = msl - 1; i > a; i--) {
 			popmenu();
 		}
 	}
+
+	// menu.onmousedown
 	function onmousedown(e) {
 		if (e.target.nodeName === "MENU") {
 			if (e.offsetX === 0) {
@@ -104,23 +139,35 @@
 		return false;
 	}
 	function onsubcontextmenuout(e) {
+		// Cancel any other previews
 		preview_show_timer = clearTimeout(preview_show_timer);
-		var menu = mother("MENU", e.toElement);
+		var menu = ancestor("MENU", e.toElement);
+		
+		// The user has deselected an expandable menu item.
+		// If the mouse has moved to the new menu, then we don't hide it:
 		if (menu === preview) {
 			return false;
 		}
+		// Othewise, we do.
 		if (preview) {
 			hideMenu(preview);
 			preview = undefined;
 		}
 	}
+
+	// menu.onmouseover: Open an expandable menu.
 	function onsubcontextmenu(e) {
+		//Create a preview
 		preview_show_timer = clearTimeout(preview_show_timer);
 		preview_show_timer = setTimeout(function () {
 			var menuitem = e.srcElement,
 				menu = menuitem.contextMenu,
 				pos;
+
+			// For later removing the [open] attribute (for styling)
 			menu.launcher = menuitem;
+			menuitem.setAttribute("open", true);
+
 			if (preview && preview !== menu) {
 				hideMenu(preview);
 			}
@@ -128,14 +175,13 @@
 			pos = offset(menuitem);
 			menu.style.top = (pos.top - 5) + "px";
 			menu.style.left = (pos.left + pos.width - 1) + "px";
-			menu.style.display = "inline-block";
-			overlay.style.display = "block";
-			if (!menuitem.opensubmenu) {
-				menuitem.setAttribute("open", true);
-			}
+			showMenu(menu);
 		}, 200);
-		//Don't stop propagation, the event bubbles to the <menu /> mouseover handler
+		// Don't stop propagation, the event bubbles to the <menu /> mouseover handler,
+		// which will hide any dead contextmenus
 	}
+
+	// Adds javascript code, so it works without native html5 contextmenu support
 	function prepareMenu(menu) {
 		var p = menu.parentNode,
 			clone;
@@ -143,6 +189,10 @@
 		menu.addEventListener("mousedown", onmousedown);
 		menu.addEventListener("contextmenu", menuoncontextmenu);
 		if (p.nodeName === "MENU") {
+			// If it is a menu within a menu, instead make it a menu which points to another menu.
+			// It would be better to keep it as menu > menu ...,
+			// but styling became too difficult so menu > menu s are replaced with menuitem.submenu
+			// And the menu > menu is moved to overlay > menu, and is set as the contextMenu property
 			clone = d.createElement("menuitem");
 			menu.classList.add("submenu");
 			clone.className =  menu.className;
@@ -166,23 +216,45 @@
 		overlay.style.display = "block";
 		holding = false;
 	}
-	function oncontextsheet(e) {
-		var pos = offset(e.target),
-			menu = d.getElementById(e.srcElement.getAttribute("contextmenu"));
-		t = new Date();
+
+	// Context menu sheets are activated through buttons instead of right clicks.
+	function oncontextsheet(e, menu, button) {
+		var pos;
+		if (button === undefined) {
+			button = e.target;
+		}
+		pos = offset(button);
+		if (menu === undefined) {
+			menu = contextMenufor(button);
+		}
 		initContextMenu(menu, pos.left, pos.top + pos.height + 8);
-		e.preventDefault();
+		if (e) {
+			e.preventDefault();
+			e.stopPropagation();
+		}
+		holding = true;
 		return false;
 	}
+	function oncontextsheetbtnup(e){
+		oncontextsheet(e);
+		holding = false;
+	}
+	// Finds the menu element associated with an element. (Searchs up the dom tree)
 	function contextMenufor(node) {
 		if (!node || !node.hasAttribute) {
 			return;
 		}
+		if (node.contextMenu) {
+			return node.contextMenu;
+		}
 		if (node.hasAttribute("contextmenu")) {
-			return node.getAttribute("contextmenu");
+			var node = d.getElementById(node.getAttribute("contextmenu"));
+			node.contextMenu = node;
+			return node;
 		}
 		return contextMenufor(node.parentNode);
 	}
+
 	function simulateClickEvent(elm, e) {
 		var evt;
 		if (document.createEvent) {
@@ -210,8 +282,10 @@
 			elm.dispatchEvent(evt);
 		}
 	}
+
+	// ([contextmenu]).oncontextmenu
 	function oncontextmenu(e) {
-		var menu = d.getElementById(contextMenufor(e.srcElement)),
+		var menu = contextMenufor(e.srcElement),
 			x = e.clientX,
 			y = e.clientY;
 		initContextMenu(menu, e.clientX, e.clientY);
@@ -225,6 +299,8 @@
 	function inititalize() {
 		menustack = [];
 		overlay = d.createElement("div");
+		
+		// Style:
 		var os_code = "osx10_7",
 			mouseup_wait_for_me = 0;
 		if (/Mac/.test(navigator.userAgent)) {
@@ -251,6 +327,8 @@
 					return false;
 				}
 				if (holding) {
+					// When doing a hold down and release style selection, a click event isn't
+					// automatticaly triggered by the browser
 					simulateClickEvent(menuitem, e);
 				}
 			}
@@ -271,25 +349,29 @@
 				}, 10);
 			}
 		});
+
+		// Prevent scrolling while context menus are active
 		overlay.addEventListener("mousewheel", function (e) {
 			e.preventDefault();
 			e.stopPropagation();
 			return false;
 		}, true);
+
+		// Attempt to begin another context menu while already in a context menu.
 		overlay.addEventListener("contextmenu", function (e) {
+			// Rememeber that the overlay covers up all other elements which
+			// may have a contextmenu associate with them.
 			overlay.style.display = "none";
 			var node = d.elementFromPoint(e.clientX, e.clientY),
-				contextmenu = contextMenufor(node),
-				menu,
+				menu = contextMenufor(node),
 				x,
 				y,
 				dx,
 				dy;
-			if (contextmenu) {
+			if (menu) {
 				overlay.style.display = "block";
 				clearTimeout(mousedown_timeout);
 				e.preventDefault();
-				menu = d.getElementById(contextmenu);
 				if (menustack[0] === menu) {
 					//Same menu:
 					x = e.clientX;
@@ -298,6 +380,9 @@
 					dy = y - lastY;
 					lastX = x;
 					lastY = y;
+					// If the mouse hasn't moved much, then ignore it.
+					// This was probably an attempt to hide the context menu,
+					// rather than initate another of exactly the same type.
 					if (dx * dx + dy * dy < 50) {
 						overlay.style.opacity = "1.0";
 						overlay.style.display = "block";
@@ -306,6 +391,9 @@
 						return;
 					}
 				}
+
+				// The mouseup event will attempt to hide all context menus.
+				// Temporarily disable it:
 				mouseup_wait_for_me++;
 				setTimeout(function () {
 					while (menustack.length) {
@@ -344,6 +432,7 @@
 			i,
 			l,
 			menus = [];
+		// Build an array, since 'NodeListPrototype's update themselves while iterating.
 		for (i = 0, l = menus_dom.length; i < l; i++) {
 			menus[i] = menus_dom[i];
 		}
@@ -358,9 +447,9 @@
 			l;
 		for (i = 0, l = linkers.length; i < l; i++) {
 			element = linkers[i];
+			element.contextMenu = d.getElementById(element.getAttribute("contextmenu"))
 			if (element.nodeName === "INPUT") {
-				element.addEventListener("click", oncontextsheet);
-				//Right clicking buttons. Bad idea?
+				element.addEventListener("mouseup", oncontextsheetbtnup);
 				element.addEventListener("contextmenu", oncontextsheet);
 			} else {
 				element.addEventListener("contextmenu", oncontextmenu);
@@ -379,6 +468,7 @@
 			xi,
 			submenu,
 			menuitem;
+		menu.setAttribute("type", "context");
 		for (i = 0, l = x.length; i < l; i++) {
 			xi = x[i];
 			if (xi.children) {
@@ -386,19 +476,26 @@
 				submenu.setAttribute("label", xi.label);
 				menu.appendChild(submenu);
 			} else {
-				menuitem = d.createElement("menuitem");
-				menuitem.setAttribute("label", xi.label);
-				if (xi.onclick) {
-					menuitem.onclick = xi.onclick;
+				if (xi.hr) {
+					menuitem = d.createElement("hr");
+				} else {
+					menuitem = d.createElement("menuitem");
+					menuitem.setAttribute("label", xi.label);
+					if (xi.onclick) {
+						menuitem.onclick = xi.onclick;
+					}
+					if (xi.icon) {
+						menuitem.icon = xi.icon;
+					}
 				}
-				if (xi.icon) {
-					menuitem.icon = xi.icon;
-				}
+				
 				menu.appendChild(menuitem);
 			}
 		}
 		return menu;
 	}
+
+	// Main API:
 	function contextmenu(x) {
 		var menu = buildMenu(x),
 			submenus,
@@ -421,14 +518,38 @@
 	}
 	contextmenu.show = function (menu, x, y) {
 		if (nativeSupport) {
-			throw ("Not supported");
+			throw ("Not supported!");
 		}
 		if (typeof menu === "string") {
 			menu = d.getElementById(menu);
+		} else {
+			menu = html(menu);
 		}
-		initContextMenu(menu, x, y);
+		if (typeof x === "number") {
+			initContextMenu(menu, x, y);
+		} else {
+			oncontextsheet(null, menu, html(x));
+			holding = false;
+		}
 		return this;
 	};
+	contextmenu.attach = function (element, menu) {
+		element = html(element);
+		menu = html(menu);
+		if(nativeSupport) {
+			menu.id = menu.id || (0 | 10000 * Math.random()).toString(30);
+			element.setAttribute("contextmenu", menu.id);
+			return;
+		}
+		element.contextMenu = menu;
+		if (element.nodeName === "INPUT") {
+			element.addEventListener("mouseup", oncontextsheetbtnup);
+			// element.addEventListener("mousedown", oncontextsheet); css :hover doesn't work
+			element.addEventListener("contextmenu", oncontextsheet);
+		} else {
+			element.addEventListener("contextmenu", oncontextmenu);
+		}
+	}
 	contextmenu.noConflict = function () {
 		window.contextmenu = old_contextmenu;
 		return contextmenu;
